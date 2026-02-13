b59e52f226f8b5cc80feb7546031746ff762cb63
DavidIncarbone <david.incarbone@gmail.com>
2026-02-13 13:18:34 +0100

    Refactor index.html structure and enhance script.js for current weather data retrieval

--- script/script.js
+++ script/script.js
@@ -1,4 +1,26 @@
 // Comandi per ridurre gli errori e migliorare la leggibilità del codice JavaScript.
 "use strict";
 console.clear();
-console.log("test");
+
+const lat = 45.4642; // Latitudine di Milano
+const lon = 9.19; // Longitudine di Milano
+const apiKey = "db84958f63f2d8e6ff20e71cbbd8040e"; // Mia API Key per OpenWeatherMap
+const lang = "it"; // Lingua italiana per le descrizioni del tempo
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=it`;
+
+getCurrentWeatherData(); // Chiamata alla funzione per recuperare i dati meteo e aggiornare la pagina
+
+// *** FUNZIONI ***
+
+// Funzione per recuperare i dati meteo e aggiornare la pagina
+function getCurrentWeatherData() {
+  axios
+    .get(apiUrl)
+    .then((res) => {
+      const data = res.data;
+      console.log(data); // Log dei dati ricevuti per debug e verifica
+    })
+    .catch((err) => {
+      console.error("Errore nel recupero dei dati meteo:", err); // Log degli errori per debug
+    });
+}
